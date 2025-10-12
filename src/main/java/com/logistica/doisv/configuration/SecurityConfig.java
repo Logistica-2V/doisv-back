13af24a5558bda505e97d03a7638c9d62a3cda95
Marcelo William <marcelo.willian@live.com>
2025-10-12 14:31:24 -0300

    Incluído a proteção das rotas de acordo com o perfil do usuário

--- src/main/java/com/logistica/doisv/configuration/SecurityConfig.java
+++ src/main/java/com/logistica/doisv/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.logistica.doisv.services.validacao.TokenAutenticacao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,8 +27,12 @@ public class SecurityConfig {
                 .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/doisv/login", "/doisv/consumidores/login", "/h2-console/**", "/swagger-ui/**", "swagger-ui.html",
-                                "v3/api-docs/**","swagger-resources/**")
-                        .permitAll()
+                                "v3/api-docs/**","swagger-resources/**").permitAll()
+                        .requestMatchers("/doisv/produtos/**").hasRole("LOJISTA")
+                        .requestMatchers("/doisv/consumidores/**").hasRole("LOJISTA")
+                        .requestMatchers("/doisv/lojas/**").hasRole("LOJISTA")
+                        .requestMatchers("/doisv/lojistas/**").hasRole("LOJISTA")
+                        .requestMatchers("/doisv/vendas/**").hasRole("LOJISTA")
                         .anyRequest().authenticated()
                 ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(tokenAutenticacao, UsernamePasswordAuthenticationFilter.class);
--- src/main/java/com/logistica/doisv/dto/AcessoDTO.java
+++ src/main/java/com/logistica/doisv/dto/AcessoDTO.java
@@ -4,6 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Data
 @Builder
@@ -24,5 +29,12 @@ public class AcessoDTO {
     public enum TipoUsuario{
         LOJISTA,
         CONSUMIDOR
+    }
+
+    public Collection<? extends GrantedAuthority> getPermissao(){
+        if(this.tipo == null){
+            return Collections.emptyList();
+        }
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + this.tipo.name()));
     }
 }
